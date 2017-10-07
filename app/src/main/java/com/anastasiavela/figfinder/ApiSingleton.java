eac7f7d674ee22166a90239fd15f3b0e8b4fa6d0
Max McArthur <max.mcarthur@berkeley.edu>
2017-10-07 14:33:59 -0700

    Started api calling, modified build

---
+++ app/src/main/java/com/anastasiavela/figfinder/ApiSingleton.java
@@ -0,0 +1,15 @@
+package com.anastasiavela.figfinder;
+
+import android.content.Context;
+
+/**
+ * Created by maxmcarthur on 10/7/17.
+ */
+
+public class ApiSingleton {
+    private static ApiSingleton mSingletonInstance;
+
+    private ApiSingleton(Context context) {
+
+    }
+}
